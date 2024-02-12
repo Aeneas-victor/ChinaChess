5dd1d32fc8b6c7b5b2cb27657504c2bead10e20b
Aeneas <15004916545@163.com>
2024-02-12 14:55:10 +0800

    First commit

---
+++ common.h
@@ -0,0 +1,18 @@
+
+#pragma once
+#include<iostream>
+#include<Windows.h>
+#include<graphics.h>
+#include<thread>
+#include<map>
+#include<math.h>
+#include<algorithm>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+#define SQU(x) ((x)*(x))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define CHESS_SIZE 70
+#define INTERVAL 50
+#define ROW 10
+#define COL 9
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include"Chess.h"
+void realize()
+{
+	Chess* chess = new Chess;
+	chess->testChessName();
+	chess->testpoint();
+	chess->testcolor();
+	chess->testaddress();
+	BeginBatchDraw();
+	while (!chess->GameOverx)
+	{
+		chess->GetControl();
+		chess->refresh();
+		FlushBatchDraw();
+	}
+	EndBatchDraw();
+	ExMessage mess;
+	while (1) {
+		if (peekmessage(&mess, EM_MOUSE))
+		{
+			if (mess.message == WM_LBUTTONDOWN)
+			{
+				break;
+			}
+		}
+	}
+
+	
+	delete chess;
+}
+int main()
+{
+	realize();
+	/*initgraph(600, 500);
+	IMAGE img;
+	loadimage(&img, "resource\\1234.jpg",600,500,true);
+	putimage(0, 0, &img);
+	while (1)
+	{
+		
+	}
+	closegraph();*/
+}
